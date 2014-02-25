d2383e1514edea2c34482aed719b9517f9f36528
Ash McKenzie <ash@greenworm.com.au>
2014-02-25 15:09:36 +1100

    We now have three crons :)

--- config/checks/reddit_emailer.rb
+++ config/checks/reddit_emailer.rb
@@ -1,6 +1,6 @@
 Stethoscope.check :reddit_emailer do |response|
   cron_entries_count = `crontab -l | grep "bin/reddit-emailer"`.chomp.split("\n").count
-  status = (cron_entries_count == 2) ? 200 : 500
+  status = (cron_entries_count == 3) ? 200 : 500
 
   response[:cron_entries_count] = cron_entries_count
   response[:status] = status
